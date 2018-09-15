2f3f806e7054e62239a91d9ed4416b498c604af3
muratuenlue <muratuenlue@gmail.com>
2018-09-15 23:38:54 +0400

    Update davet.js

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -13,7 +13,7 @@ exports.run = (client, message) => {
     .setColor(0x00AE86)
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('İşte Davet Linkim: https://discordapp.com/api/oauth2/authorize?client_id=490458079637536769&permissions=8&scope=bot Buda Bizim Sunucunun :)  https://discord.gg/h9PRT7h');
+    .setDescription('İşte Davet Linkim: https://discordapp.com/api/oauth2/authorize?client_id=490458079637536769&permissions=8&scope=bot Buda Bizim Sunucunun :)  https://discord.gg/duPXaZR');
     return message.author.sendEmbed(pingozel)
 };
 
