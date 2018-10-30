a872447fd341bc3cebe1e891a7942996bc349351
muratuenlue <muratuenlue@gmail.com>
2018-10-30 11:48:52 +0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,5 +9,8 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-client.user.setGame("?yardım | " + client.guilds.size + " Sunucu " + client.users.size + " Kullanıcı ", );
-}
+  client.user.setGame(`Bot yayında yeyeyye ${prefix}yardım + ${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+};
+
