70dcc14c118abe50dc87db7dccce24f1032c015d
muratuenlue <muratuenlue@gmail.com>
2018-11-09 13:14:03 +0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,16 +5,12 @@ const ayarlar = require('../ayarlar.json');
 
 var prefix = ayarlar.prefix;
 
-let statuses = ['https://discord.gg/bmbSrqu', 'Selam Dunya']
-clint.on('ready',() => {
-
-  let statuses = statuses[Math.floor(Math.random()*statuses.lenght)];
-
-  client.user.setPresence({ activity: {yeni ozellik}, status: 'online'});
-  client.user.setPresence({ activity: {selamlar}, status: 'online'});
-
-    setInterval(function(){
-
-    }, 10000)
-
-        })
+module.exports = client => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("online");
+  client.user.setGame(`${prefix}yardım + ${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
+client.user.presentGame(`denemee`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+};
