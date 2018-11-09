423bac1e854929001c0f07d9532dd5cbcbfac3b6
muratuenlue <muratuenlue@gmail.com>
2018-11-09 14:35:05 +0400

    Update surelisusutur.js

--- komutlar/surelisusutur.js
+++ komutlar/surelisusutur.js
@@ -4,11 +4,11 @@ const client = new Discord.Client();
 
 exports.run = async (client, message, args) => {
 
-  //!geçicisustur@üye 1s/m/h/d | 1s = 1 saniye , 1m = 1 dakika , 1h = 1 saat, 1d = 1 gün
+  //!süreli-sustur@üye 1s/m/h/d | 1s = 1 saniye , 1m = 1 dakika , 1h = 1 saat, 1d = 1 gün
 
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!tomute) return message.reply("Bu Komutu Kullanmak İçin : +geçici-sustur <Kullanıcı> <Süre> Olarak Yazmalısınız.");
-  if(tomute.hasPermission("VIEW_AUDIT_LOG")) return message.reply("Hata: Geçici olarak susturmaya çalıştığınız kişi yetkili veya bot'un yetkisi belirttiğiniz kişiyi geçici olarak susturmaya yetmiyor!");
+  if(!tomute) return message.reply("Yanlış yazdınız doğru yazım:b/süreli-sustur <kullanıcı> <süre> ");
+  if(tomute.hasPermission("VIEW_AUDIT_LOG")) return message.reply("Bu kişi yetkilidir");
 let muterole = message.guild.roles.find(r => r.name === "Muted");
 
   if(!muterole){
@@ -30,10 +30,10 @@ let muterole = message.guild.roles.find(r => r.name === "Muted");
   }
   //end of create role
   let mutetime = args[1];
-  if(!mutetime) return message.reply("Bu Komutu Kullanmak İçin : +geçici-sustur <Kullanıcı> <Süre> Olarak Yazmalısınız.");
+  if(!mutetime) return message.reply("Yanlış yazdınız doğru yazım:b/süreli-sustur <kullanıcı> <süre>");
 
   await(tomute.addRole(muterole.id));
-  message.reply(`<@${tomute.id}> susturuldu! ${ms(ms(mutetime))}`);
+  message.reply(`<@${tomute.id}> başarıyla susturuldu! ${ms(ms(mutetime))}`);
 
   setTimeout(function(){
     tomute.removeRole(muterole.id);
@@ -47,12 +47,12 @@ let muterole = message.guild.roles.find(r => r.name === "Muted");
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ['geçici-sustur', 'gsustur'],
+  aliases: ['süreli-sustur', 'tempmute'],
   permLevel: 1
 };
 
 exports.help = {
-  name: 'geçici-sustur',
-  description: 'Sureli Susturur.',
-  usage: 'geçici-sustur [Kullanıcı] [Süre]'
+  name: 'süreli-sustur',
+  description: 'Modların ve üstündeki kişilerin 2 tane uyarı alanları süreli susturmasına yarar.',
+  usage: 'süreli-sustur [Kullanıcı] [Süre]'
 };
