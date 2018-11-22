bbe401c86dea784286a9efb77409844b8d33bdcc
muratuenlue <muratuenlue@gmail.com>
2018-11-22 22:49:44 +0400

    Update davet.js

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -5,7 +5,7 @@ var prefix = ayarlar.prefix;
 
 exports.run = (client, message, params) => {
 
-  const embedyardim = new Discord.RichEmbed()
+  const embeddavet = new Discord.RichEmbed()
   .setDescription(`[Bot Davet Linki](  https://discordapp.com/api/oauth2/authorize?client_id=506402962906677271&permissions=8&scope=bot  ) | [Destek Sunucusu](  https://discord.gg/Jg6Dbcr  )\n\n**Ping:** ${Math.round(client.ping)}ms!`)
   .setColor("#7289DA")
   .setFooter(`${message.author.username} tarafından istendi.`, message.author.avatarURL)
