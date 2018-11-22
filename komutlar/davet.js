2d7f75f9e2aa6c504b922bd3ce450648b1d130b8
muratuenlue <muratuenlue@gmail.com>
2018-11-22 22:47:54 +0400

    Update davet.js

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -10,28 +10,16 @@ exports.run = (client, message, params) => {
   .setColor("#7289DA")
   .setFooter(`${message.author.username} tarafından istendi.`, message.author.avatarURL)
 
-  if (!params[0]) {
-    const commandNames = Array.from(client.commands.keys());
-    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.channel.send(embedyardim);
-  } else {
-    let command = params[0];
-    if (client.commands.has(command)) {
-      command = client.commands.get(command);
-      message.author.send('asciidoc', `= ${command.help.name} = \n${command.help.description}\nDoğru kullanım: ` - prefix - `${command.help.usage}`);
-    }
-  }
-};
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['h', 'halp', 'help', 'y'],
+  aliases: ['d', 'botu-ekle', 'bote', 'sunucuya-ekle'],
   permLevel: 0
 };
 
 exports.help = {
-  name: 'yardım',
-  description: 'Tüm komutları gösterir.',
-  usage: 'yardım [komut]'
+  name: 'davet',
+  description: 'Botun davet linkini atar',
+  usage: 'davet'
 };
