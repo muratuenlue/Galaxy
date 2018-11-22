388f7a14c4df35313e64928189b3d915008e0990
muratuenlue <muratuenlue@gmail.com>
2018-11-22 22:19:53 +0400

    Update davet.js

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -1,31 +1,37 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const ayarlar = require('../ayarlar.json');
 
-exports.run = (client, message) => {
-  if (message.channel.type !== 'dm') {
-    const ozelmesajkontrol = new Discord.RichEmbed()
-    .setColor(0x00AE86)
-    .setTimestamp()
-    .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('Özel mesajlarını kontrol et. :postbox:');
-    message.channel.sendEmbed(ozelmesajkontrol) }
-	const pingozel = new Discord.RichEmbed()
-    .setColor(0x00AE86)
-    .setTimestamp()
-    .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('İşte Davet Linkim: https://discordapp.com/api/oauth2/authorize?client_id=506402962906677271&permissions=8&scope=bot Buda Bizim Sunucunun :) https://discord.gg/bmbSrqu');
-    return message.author.sendEmbed(pingozel)
+var prefix = ayarlar.prefix;
+
+exports.run = (client, message, params) => {
+
+  const embedyardim = new Discord.RichEmbed()
+  .setDescription(`[Bot Davet Linki](  https://discordapp.com/api/oauth2/authorize?client_id=506402962906677271&permissions=8&scope=bot  ) | [Destek Sunucusu](  https://discord.gg/Jg6Dbcr  )\n\n**Ping:** ${Math.round(client.ping)}ms!`)
+  .setColor("#7289DA")
+  .setFooter(`${message.author.username} tarafından istendi.`, message.author.avatarURL)
+
+  if (!params[0]) {
+    const commandNames = Array.from(client.commands.keys());
+    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+    message.channel.send(embedyardim);
+  } else {
+    let command = params[0];
+    if (client.commands.has(command)) {
+      command = client.commands.get(command);
+      message.author.send('asciidoc', `= ${command.help.name} = \n${command.help.description}\nDoğru kullanım: ` - prefix - `${command.help.usage}`);
+    }
+  }
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['botu ekle', 'botu davet et', 'botuekle', 'invite'],
+  aliases: ['h', 'halp', 'help', 'y'],
   permLevel: 0
 };
 
 exports.help = {
-  name: 'davet',
-  description: 'Botun davet linkini gönderir.',
-  usage: 'davet'
+  name: 'yardım',
+  description: 'Tüm komutları gösterir.',
+  usage: 'yardım [komut]'
 };
